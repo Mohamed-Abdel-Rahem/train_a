e35f6203a1213827512745eb4a1c6c54aa885fcb
Mohamed-Abdel-Rahem <mohammedabdelrahem202123067@gmail.com>
2024-11-23 17:42:35 +0200

    class user to equall two object without any package

--- lib/screens/home_view.dart
+++ lib/screens/home_view.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
   @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  List<Color> colors = [
+    Colors.pink,
+    Colors.green,
+    Colors.red,
+    Colors.blue,
+    Colors.orange,
+  ];
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: ListView.builder(
+          itemCount: colors.length,
+          itemBuilder: (context, index) => Container(
+                height: 300,
+                color: colors[index],
+              )),
+    );
   }
 }
